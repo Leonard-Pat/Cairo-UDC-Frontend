933311e008b2895c561f0122b9b094c420c603f2
Leonard Paturel <leonardpaturel@gmail.com>
2024-02-12 10:22:11 +0000

    shadcn ui

---
+++ app/state/wallet.ts
@@ -0,0 +1,54 @@
+import { create } from "zustand";
+import type { ConnectedStarknetWindowObject } from "get-starknet-core";
+
+type WalletStore = {
+  account?: ConnectedStarknetWindowObject["account"];
+  connecting: boolean;
+  connect: () => Promise<ConnectedStarknetWindowObject["account"]>;
+  reconnect: () => Promise<void>;
+  disconnect: () => void;
+};
+
+export const useWalletStore = create<WalletStore>((set) => ({
+  connecting: true,
+  connect: async () => {
+    try {
+      const { connect } = await import("starknetkit");
+      const wallet = await connect({
+        modalMode: "alwaysAsk",
+      });
+      if (!wallet || !wallet.wallet?.account) {
+        throw new Error("No account found");
+      }
+      set({ account: wallet.wallet.account, connecting: false });
+    } catch (e) {
+      set({ connecting: false });
+      throw e;
+    }
+  },
+  reconnect: async () => {
+    try {
+      const { connect } = await import("starknetkit");
+      const wallet = await connect({
+        modalMode: "neverAsk",
+      });
+      if (!wallet?.wallet?.account) {
+        throw new Error("No account found");
+      }
+      set({ account: wallet.wallet.account, connecting: false });
+    } catch (e) {
+      set({ connecting: false });
+      throw e;
+    }
+  },
+  disconnect: async () => {
+    try {
+      const { disconnect } = await import("starknetkit");
+      await disconnect({ clearLastWallet: true });
+      set({ account: undefined, connecting: false });
+    } catch (e) {
+      set({ connecting: false });
+      throw e;
+    }
+  },
+}));
